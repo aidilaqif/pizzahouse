30cf4428a54b917e9c3fd41d80a5cd2238710936
AIDIL 'AQIF MOHD PIDAUS <92839227+aidilaqif@users.noreply.github.com>
2024-09-30 17:20:31 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pizzas', function(){
+    return view('pizzas');
+    // return ("pizza");
+    // return ['name' => 'veg pizzas', 'base' => 'classic'];
+});
