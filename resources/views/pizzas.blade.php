b4043a7fbc39ab88bc70e1eba18444bbd10b7929
AIDIL 'AQIF MOHD PIDAUS <92839227+aidilaqif@users.noreply.github.com>
2024-10-01 11:07:00 +0800

    Update pizzas.blade.php

--- resources/views/pizzas.blade.php
+++ resources/views/pizzas.blade.php
@@ -69,27 +69,29 @@
                 <div class="title m-b-md">
                     Pizza List
                 </div>
-                <p>{{$type}} - {{$base}} - {{$price}}</p>
                 
-                {{-- if else in blade --}}
-                @if($price > 15)
-                    <p>this pizza is expensive</p>
-                @elseif ($price < 5)
-                    <p>this pizza is cheap</p>
-                @else
-                    <p>this pizza is normally priced</p>
-                @endif
+                
+                {{-- for loop in blade --}}
+                {{-- @for ($i = 0; $i < 5; $i++)
+                    <p>the value of i is {{$i}}</p>
+                @endfor --}}
 
-                {{-- opposite if else it will output if false --}}
-                @unless ($base == 'cheesy crust')
-                    <p>you don't have a cheesy crust</p>
-                @endunless
+                {{-- @for ($i = 0; $i < count($pizzas); $i++)
+                    <p>{{$pizzas[$i]['type']}}</p>
+                @endfor --}}
 
-                {{-- output regular php; php is bigger n more things can do; --}}
-                @php
-                    $name = 'shaul';
-                    echo($name);
-                @endphp
+                {{-- parameter after 'as' can be called whatever we want --}}
+                @foreach ($pizzas as $pizza)
+                    <div>
+                        {{$loop -> index}} {{$pizza['type']}} - {{$pizza['base']}}
+                        @if ($loop -> first)
+                            <span>- first in the loop</span>
+                        @endif
+                        @if ($loop -> last)
+                            <span>- last in the loop</span>
+                        @endif
+                    </div>
+                @endforeach
             </div>
         </div>
     </body>
