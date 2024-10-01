68828d7cb7974b1df13dfc3422968b0b9b0de39a
AIDIL 'AQIF MOHD PIDAUS <92839227+aidilaqif@users.noreply.github.com>
2024-10-01 11:06:56 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,12 @@ Route::get('/', function () {
 
 Route::get('/pizzas', function(){
     // get data from db, then pass into array
-    $pizza = [
-        'type' => 'hawaiian',
-        'base' => 'garlic',
-        'price' => 10
+    $pizzas = [
+        ['type' => 'hawaiian', 'base' => 'cheesy crust'],
+        ['type' => 'volcano', 'base' => 'garlic crust'],
+        ['type' => 'veg supreme', 'base' => 'thin & crispy'],
     ];
-    return view('pizzas',$pizza);
+
+    return view('pizzas',['pizzas' => $pizzas]);
     
 });
