060c091688e9502b838126e14f8f06bd9b23c72b
AIDIL 'AQIF MOHD PIDAUS <92839227+aidilaqif@users.noreply.github.com>
2024-10-02 17:34:15 +0800

    Update PizzaController.php

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -3,16 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Pizza;
 
 class PizzaController extends Controller
 {
     public function index(){
-        // get data from db, then pass into array
-        $pizzas = [
-            ['type' => 'hawaiian', 'base' => 'cheesy crust'],
-            ['type' => 'volcano', 'base' => 'garlic crust'],
-            ['type' => 'veg supreme', 'base' => 'thin & crispy'],
-        ];
+        // get all data from db, then pass into array
+        // $pizzas = Pizza::all();
+
+        // get all data from db but order them by name, then pass into array
+        // $pizzas =Pizza::orderBy('name')->get();
+
+        // get match data from db, then pass into array
+        // $pizzas = Pizza::where('type','hawaiian')->get();
+
+        // get latest data only
+        $pizzas = Pizza::latest()->get();
 
         return view('pizzas',[
             'pizzas' => $pizzas,
