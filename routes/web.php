8867511fcdaa5fdad0d11f700402cae90bad1853
AIDIL 'AQIF MOHD PIDAUS <92839227+aidilaqif@users.noreply.github.com>
2024-10-02 21:35:26 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// route is by order
 // route for /pizzas
 Route::get('/pizzas', [PizzaController::class,'index']);
+Route::get('/pizzas/create',[PizzaController::class,'create']);
 Route::get('/pizzas/{id}', [PizzaController::class,'show']);//controllerName@function
