d3149e540a0f61480e8d22017559dd00f28c8a13
AIDIL 'AQIF MOHD PIDAUS <92839227+aidilaqif@users.noreply.github.com>
2024-10-02 21:35:17 +0800

    Create create.blade.php

---
+++ resources/views/pizzas/create.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.layout')
+
+@section('content')
+<body class="font-sans antialiased dark:bg-black dark:text-white/50">
+<div class="flex-center position-ref full-height">
+    <div class="content">
+        <div class="title m-b-md">
+            Add a New Pizza
+        </div>
+        
+    </div>
+</div>
+@endsection
