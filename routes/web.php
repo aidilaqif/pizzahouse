bd89bebd8bae624647f10488492e5154ee743329
AIDIL 'AQIF MOHD PIDAUS <92839227+aidilaqif@users.noreply.github.com>
2024-10-03 16:32:58 +0800

    laravel auth setup

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@ Route::get('/pizzas/create',[PizzaController::class,'create']);
 Route::post('/pizzas',[PizzaController::class, 'store']);
 Route::get('/pizzas/{id}', [PizzaController::class,'show']);//controllerName@function
 Route::delete('/pizzas/{id}',[PizzaController::class, 'destroy']);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
