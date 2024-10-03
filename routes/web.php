6ad59f2db4fb6ddfbd0029bf0dec406dab248399
AIDIL 'AQIF MOHD PIDAUS <92839227+aidilaqif@users.noreply.github.com>
2024-10-03 11:46:00 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,4 @@ Route::get('/pizzas', [PizzaController::class,'index']);
 Route::get('/pizzas/create',[PizzaController::class,'create']);
 Route::post('/pizzas',[PizzaController::class, 'store']);
 Route::get('/pizzas/{id}', [PizzaController::class,'show']);//controllerName@function
+Route::delete('/pizzas/{id}',[PizzaController::class, 'destroy']);
